038114625498b5c1e729e141aa62cbf01a41c9a9
Marty Sirkin <marty@sirkin.net>
2020-10-05 14:31:27 -0700

    Cleaned up and documented

--- Swiftly/Program.cs
+++ Swiftly/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿
+using System;
 using System.Collections;
+using System.Threading;
 
 namespace Swiftly
 {
@@ -23,10 +25,16 @@ namespace Swiftly
     /*** AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION   ***/
     /*** WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                   ***/
 
-    class Program
+   // NOTE: Originally, I was going to break up this file into smaller files (proper large-project structuring)
+   //        with Partial Classes (pretty standard C#). This file is short enough, though, that 
+   //        I decided to leave it as one source file to make it easier for you to read through.
+
+    class Swiftly
     {
         /*********************************************************************************/
         /***     Constants                                                             ***/
+        /***                                                                           ***/
+        /***     Library: None Used                                                    ***/
         /***                                                                           ***/
         /***     Copyright 2020 MAJE Software. All Rights Reserved.                    ***/
         /*********************************************************************************/
@@ -39,17 +47,49 @@ namespace Swiftly
 
         const double GLOBAL_TAX_RATE = .075;
 
+        const bool TRUE = true;
+
         const int NUM_PRODUCTS = 10;
+        //const int SLEEP_PERIOD = 60000;       // Wake up every 60 seconds
+        const int SLEEP_PERIOD = 5000;          // Wake up every 5 seconds
+
+        // Error/reporting constants
+        const int LOG_NO_LOG = 0;
+        const int LOG_FATAL = 32;
+        const int LOG_ERROR = 16;
+        const int LOG_WARN = 8;
+        const int LOG_INFO = 4;
+        const int LOG_DEBUG = 2;
+        const int LOG_TRACE = 1;
+
+        const int LOG_TRACE_DEBUG_INFO = 7;
+        const int ALL_FLAGS = 63;
+
+        // Messaging strings
+        // NOTE: For a production application, these should be internationaized/localized
+        const string NOTIFY_SLEEP_MSG = "Going to sleep...";
+        const string NOTIFY_WAKE_MSG = "Waking up...";
+        const string NUM_PROD_LOADED_MSG = "Number of products loaded: ";
+        const string CANNOT_LOAD_INPUT_FILE = "ERROR! Cannot load input file: ";
+        const string INPUT_FILE_NAME_PATH = "C:\\Users\\marty\\Source\\Repos\\Swiftly\\Swiftly\\input.txt";
+        const string INPUT_FILE_LINE_NOT_PARSED = "Input file could not be parsed at line: ";
 
         /*********************************************************************************/
         /***     Global Variables                                                      ***/
         /***                                                                           ***/
+        /***     Library: None Used                                                    ***/
+        /***                                                                           ***/
         /***     Copyright 2020 MAJE Software. All Rights Reserved.                    ***/
         /*********************************************************************************/
-        static ArrayList prodList = new ArrayList();
+        static ArrayList PROD_LIST = new ArrayList();              // Array list of products
+        static int ALL_INFO_LOG = 15;
+
+        static int CURRENT_NOTIFY_LEVEL = ALL_INFO_LOG;            // Current error reporting level
 
         /*********************************************************************************/
         /***     Product Class Type                                                    ***/
+        /***                                                                           ***/
+        /***     Library: None Used                                                    ***/
         /***                                                                           ***/
         /***     Copyright 2020 MAJE Software. All Rights Reserved.                    ***/
         /*********************************************************************************/
@@ -107,6 +147,8 @@ namespace Swiftly
         /*********************************************************************************/
         /***     Parse Input String                                                    ***/
         /***                                                                           ***/
+        /***     Library: None Used                                                    ***/
+        /***                                                                           ***/
         /***     Copyright 2020 MAJE Software. All Rights Reserved.                    ***/
         /*********************************************************************************/
         static Product parseInputString(string line)
@@ -157,14 +199,36 @@ namespace Swiftly
         /*********************************************************************************/
         /***     Load Input Data                                                       ***/
         /***                                                                           ***/
+        /***     Library: None Used                                                    ***/
+        /***                                                                           ***/
         /***     Copyright 2020 MAJE Software. All Rights Reserved.                    ***/
         /*********************************************************************************/
         static void loadInputFile()
         {
             int intLineNum = 1;
+            string[] lines = null;
 
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\marty\Source\Repos\Swiftly\Swiftly\input.txt");
+            // Open up and read the lines from the product catalog
+            // NOTE: If the catalogs get too big, perhaps parse one line at a time to reduce memory allocation
+ 
+            // NOTE: For this test, we are only loading one input file, which doesn't change (in this case)
+            //       For a real load, we should investigate identifying and only loading the delta changes
+            try
+            {
+                lines = System.IO.File.ReadAllLines(@INPUT_FILE_NAME_PATH);
+            } catch
+            {
+                Notify(CANNOT_LOAD_INPUT_FILE + INPUT_FILE_NAME_PATH, ALL_FLAGS);
+                return;
+            }
 
+            // NOTE: In a real system, we could/should read the store number (and hence the region/store) from either the title
+            //        of the input file, or some data near the top of the data file. 
+            //
+            //       For this simple illustration, there is only one input file and the code has no concern about multiple price files because
+            //        this simulation is ALSO not tracking a shopping cart.
+
+            // For all of the prices in the catalog for this store...
             foreach (string line in lines)
             {
 
@@ -172,25 +236,65 @@ namespace Swiftly
 
                 if (newProduct == null)
                 {
-                    Console.WriteLine("Input line #{0} could not be parsed: " + line, intLineNum++);
-                }
+                    Notify(INPUT_FILE_LINE_NOT_PARSED + intLineNum++, ALL_INFO_LOG);
+                 }
                 else
                 {
-                    prodList.Add(newProduct);
+                    PROD_LIST.Add(newProduct);
                 }
+            }
+        }
+
+        /*********************************************************************************/
+        /***     Notify User                                                           ***/
+        /***                                                                           ***/
+        /***     Library: None Used                                                    ***/
+        /***                                                                           ***/
+        /***     Copyright 2020 MAJE Software. All Rights Reserved.                    ***/
+        /*********************************************************************************/ 
+        static void Notify(string reportString, int notifyLevel)
+        {
+            if ((CURRENT_NOTIFY_LEVEL & notifyLevel) > 0)            {
+                Console.WriteLine(reportString);
             }
         }
 
         /*********************************************************************************/
         /***     Main Routine                                                          ***/
         /***                                                                           ***/
+        /***     Library: None Used                                                    ***/
+        /***                                                                           ***/
         /***     Copyright 2020 MAJE Software. All Rights Reserved.                    ***/
         /*********************************************************************************/
         static void Main(string[] args)
         {
-            loadInputFile();
+            while (TRUE)
+            {
+                // Each time through we would clean out the memory for the pricelist and load it again....
+                PROD_LIST = new ArrayList();
 
-            Console.WriteLine("Number of products loaded: {0}", prodList.Count);
+                // Load the input file
+                // Write the results
+                // *IF* there were any rows not loaded...
+                //    Notify the proper IT personnel of this issue.
+                // Sleep for the pre-defined amount of time
+                //
+                loadInputFile();
+                Notify(NUM_PROD_LOADED_MSG + PROD_LIST.Count.ToString(), LOG_TRACE_DEBUG_INFO);
+
+                // NOTE: This is only for this quick demo. In the real world we would use an Azure daemon and have it:
+                //       1. Start up however often it is configured for
+                //       2. Write out data (timestamp) upon successful conclusion
+                //       3. Have another daemon process start to insure that this daemon has run in a reasonable timeframe, or generate an error to the users
+                //       In this test only, we're just waking up at a pre-defined period
+
+                Notify(NOTIFY_SLEEP_MSG, LOG_TRACE_DEBUG_INFO);
+                Thread.Sleep(SLEEP_PERIOD);
+                Notify(NOTIFY_WAKE_MSG, LOG_TRACE_DEBUG_INFO);
+            }
+
+            // NOTE: For a real product, must also have a memory check to insure no memory leakage
+            //   (This would be less of an issue with a proper daemon waking up, processing the data, and terminating
         }
     }
 }
